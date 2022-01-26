e93e82cbd740bd42139a6ad14aeb3f53e9d4bb1f
meiraa :) <98461224+mangaliits@users.noreply.github.com>
2022-01-26 12:06:59 +0000

    izpildiju 2.uzd

---
+++ main.py
@@ -0,0 +1,12 @@
+import math
+kmg=float(input("ievadi kvadrāta malas garumu: "))
+i=5
+if kmg<5:
+  print("neeeeeee, neder. mazins kvadratins")
+else:
+  kvl= math.pow(kmg,2)
+  print("kvadrata laukums:", kvl)
+  newkv=kmg+5
+  newkvl= math.pow(newkv,2)
+  print("jaunā kv lauk.: ", newkvl)
+print("jaunais kv palielinajās par:", newkvl*100/kvl, "%")
